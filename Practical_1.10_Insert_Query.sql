b50a264cdd2804d55212e90f225528b297ca9d74
Maroot Shah <shahmaroot447@gmail.com>
2026-08-05 21:16:12 +0530

    Practical 1.10 Insert Query

---
+++ Practical_1.10_Insert_Query.sql
@@ -0,0 +1,8 @@
+INSERT INTO order_items
+(order_id, item_id, product_id, quantity, unit_price)
+VALUES
+(1, 1, 1, 2, 55000),
+(2, 1, 2, 5, 4500),
+(3, 1, 3, 10, 800),
+(4, 1, 4, 20, 600),
+(5, 1, 5, 8, 1200);
