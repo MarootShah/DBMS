185b582958870f4744bd3b55bdfc6307c58713cb
Maroot Shah <shahmaroot447@gmail.com>
2026-08-05 21:20:27 +0530

    Practical 2.2 Insert Query

---
+++ Practical_2.2_Insert_Query.sql
@@ -0,0 +1,7 @@
+INSERT INTO warehouse_copy (warehouse_id, warehousename, location_id)
+VALUES
+(6, 'Central Warehouse', 1),
+(7, 'East Warehouse', 2),
+(8, 'West Warehouse', 3),
+(9, 'South Warehouse', 4),
+(10, 'North Warehouse', 5);
