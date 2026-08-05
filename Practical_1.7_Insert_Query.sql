02554150192716775b04825a800f604d12502a5a
Maroot Shah <shahmaroot447@gmail.com>
2026-08-05 21:11:54 +0530

    Practical 1.7 Insert Query

---
+++ Practical_1.7_Insert_Query.sql
@@ -0,0 +1,8 @@
+INSERT INTO products
+(productname, description, standardcost, listprice, category_id)
+VALUES
+('Laptop', 'Dell Laptop', 45000, 55000, 1),
+('Office Chair', 'Comfortable Office Chair', 3000, 4500, 2),
+('Wireless Mouse', 'Bluetooth Mouse', 500, 800, 3),
+('T-Shirt', 'Cotton T-Shirt', 300, 600, 4),
+('Football', 'Professional Football', 800, 1200, 5);
