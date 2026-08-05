a5cc5f36f2dbc6ab185779e48408c2c45d07c7f3
Maroot Shah <shahmaroot447@gmail.com>
2026-08-05 21:07:06 +0530

    Practical 1.3 Insert Query

---
+++ Practical_1.3_Insert_Query.sql
@@ -0,0 +1,6 @@
+INSERT INTO locations (address, postalcode, city, state, country_id) VALUES
+('Alkapuri', '390007', 'Vadodara', 'Gujarat', 'IN'),
+('Alexanderplatz', '10178', 'Berlin', 'Berlin', 'DE'),
+('5th Avenue', '10001', 'New York', 'New York', 'US'),
+('Long Street', '8001', 'Cape Town', 'Western Cape', 'ZA'),
+('George Street', '2000', 'Sydney', 'New South Wales', 'AU');
