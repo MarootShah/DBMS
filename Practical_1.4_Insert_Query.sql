a6e4816bbbea20ee65ba7899777c80849817c3d1
Maroot Shah <shahmaroot447@gmail.com>
2026-08-05 21:08:26 +0530

    Practical 1.4 Insert Query

---
+++ Practical_1.4_Insert_Query.sql
@@ -0,0 +1,6 @@
+INSERT INTO warehouses (warehousename, location_id) VALUES
+('Asia Warehouse', 1),
+('Europe Warehouse', 2),
+('North America Warehouse', 3),
+('Africa Warehouse', 4),
+('Oceania Warehouse', 5);
