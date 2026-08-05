9375d1d1004ad29665d7385b0922f4f62143722b
Maroot Shah <shahmaroot447@gmail.com>
2026-08-05 21:13:07 +0530

    Practical 1.8 Insert Query

---
+++ Practical_1.8_Insert_Query.sql
@@ -0,0 +1,8 @@
+INSERT INTO customers
+(name, address, website, creditlimit)
+VALUES
+('ABC Pvt Ltd', 'Vadodara', 'www.abc.com', 100000),
+('XYZ Enterprises', 'Berlin', 'www.xyz.com', 150000),
+('Global Tech', 'New York', 'www.globaltech.com', 250000),
+('Cape Traders', 'Cape Town', 'www.capetraders.com', 180000),
+('Sydney Stores', 'Sydney', 'www.sydneystores.com', 220000);
