aa49012cc7ac466187e127f269607d903ba58fe0
Maroot Shah <shahmaroot447@gmail.com>
2026-08-05 21:14:30 +0530

    Practical 1.9 Insert Query

---
+++ Practical_1.9_Insert_Query.sql
@@ -0,0 +1,8 @@
+INSERT INTO orders
+(status, customer_id, salesman_id, order_date)
+VALUES
+('Pending', 1, 2, '2025-01-10'),
+('Shipped', 2, 3, '2025-01-12'),
+('Delivered', 3, 4, '2025-01-15'),
+('Pending', 4, 5, '2025-01-18'),
+('Delivered', 5, 2, '2025-01-20');
